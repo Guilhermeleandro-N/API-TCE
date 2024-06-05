6bc2ae0dfb5303f384e244837181a11b42493857
Guilherme Leandro <guilhermesi2005z@gmail.com>
2024-06-05 19:20:09 -0300

    Otimização no código

--- main.py
+++ main.py
@@ -63,37 +63,18 @@ def retorna_o_codigo_do_municipio_usando_o_nome(nome):
 
 #retorna informações de contas bancarias como mes e valor de abertura, usa-se o código do município e ano como parâmetros
 @app.get("/codigo_municipio={codigo_municipio}&ano={ano_exercicio}",tags=["Informações bancárias"], description="Adicione “00” ao final do ano(2007 a 2024). Exemplo: 2010 → 201000.")
-def retorna_informações_bancarias_de_municipios(codigo_municipio ,ano_exercicio):
+def retorna_informaçoes_bancarias_de_municipios(codigo_municipio ,ano_exercicio):
+    meses = {
+    1: "Janeiro",2: "Fevereiro",3: "Março",4: "Abril",5: "Maio",6: "Junho",7: "Julho",8: "Agosto",9: "Setembro",10: "Outubro",11: "Novembro",12: "Dezembro"
+    }
     nome_municipio=Retorna_o_nome_do_municipio_usando_o_codigo(codigo_municipio)["dados"][0]["nome_municipio"]
     dados=requests.get(f"https://api-dados-abertos.tce.ce.gov.br/contas_bancarias?codigo_municipio={codigo_municipio}&exercicio_orcamento={ano_exercicio}")
     dados=dados.json()
     retorno={"dados":[]}
     for c in range(len(dados["data"])):
         #o ano e mes vem no formato 202402, ano = 2024 e mes= 02.
-        if (dados["data"][c]["data_referencia"]%100) == 1:
-            mes_abertura = "Janeiro"
-        elif dados["data"][c]["data_referencia"]%100 == 2:
-            mes_abertura = "Fevereiro"
-        elif dados["data"][c]["data_referencia"]%100 == 3:
-            mes_abertura = "Março"
-        elif dados["data"][c]["data_referencia"]%100 == 4:
-            mes_abertura = "Abril"
-        elif dados["data"][c]["data_referencia"]%100 == 5:
-            mes_abertura = "Maio"
-        elif dados["data"][c]["data_referencia"]%100 == 6:
-            mes_abertura = "Junho"
-        elif dados["data"][c]["data_referencia"]%100 == 7:
-            mes_abertura = "Julho"
-        elif dados["data"][c]["data_referencia"]%100 == 8:
-            mes_abertura = "Agosto"
-        elif dados["data"][c]["data_referencia"]%100 == 9:
-            mes_abertura = "Setembro"
-        elif dados["data"][c]["data_referencia"]%100 == 10:
-            mes_abertura = "Outubro"
-        elif dados["data"][c]["data_referencia"]%100 == 11:
-            mes_abertura = "Novembro"
-        elif dados["data"][c]["data_referencia"]%100 == 12:
-            mes_abertura = "Dezembro"
+        if dados["data"][c]["data_referencia"]%100 >= 1 and dados["data"][c]["data_referencia"]%100<=12:
+            mes_abertura = meses[dados["data"][c]["data_referencia"]%100]
         else:
             mes_abertura = "Mês inválido"
         novo_dicionario={
@@ -104,7 +85,29 @@ def retorna_informações_bancarias_de_municipios(codigo_municipio ,ano_exercici
         }
         retorno["dados"].append(novo_dicionario)
     return retorno
+#retorna as contas com menor valor de abertura no mês do ano específicado
+@app.get("/valor_de_abertura?menor_codigomunicipio={codigo_municipio}&ano={ano_exercicio}&mes={mes_numerico}")
+def retorna_as_contas_com_o_menor_valor_de_abertura_no_mes(codigo_municipio,ano_exercicio,mes_numerico:int):
+    meses={
+    1: "Janeiro",2: "Fevereiro",3: "Março",4: "Abril",5: "Maio",6: "Junho",7: "Julho",8: "Agosto",9: "Setembro",10: "Outubro",11: "Novembro",12: "Dezembro"
+    }
+    retorno={"dados":[]}
+    saldos_de_abertura=[]
+    dados=retorna_informaçoes_bancarias_de_municipios(codigo_municipio ,ano_exercicio)
     
+    for c in range(len(dados["dados"])):
+        if dados["dados"][c]["mes_abertura"]==meses[mes_numerico]:
+            saldos_de_abertura.append(dados["dados"][c]["valor_saldo_abertura"])
+    saldos_de_abertura.sort()
+    menor_saldo_de_abertura=saldos_de_abertura[0]
+    for c in range(len(dados["dados"])):
 
-
-    
+        if dados["dados"][c]["valor_saldo_abertura"]==menor_saldo_de_abertura and dados["dados"][c]["mes_abertura"]==meses[mes_numerico]:
+            novo_dicionario={
+            "nome_municipio":dados["dados"][c]["nome_municipio"],
+            "codigo_municipio":dados["dados"][c]["codigo_municipio"],
+            "mes_abertura":dados["dados"][c]["mes_abertura"],
+            "valor_saldo_abertura":dados["dados"][c]["valor_saldo_abertura"]
+        }
+            retorno["dados"].append(novo_dicionario)
+    return retorno
